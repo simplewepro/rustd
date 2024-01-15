a8cce3afdc70fb66efa03ecbf0563400ee6c14e3
Alexander Malinin <podhvatovshurik@gmail.com>
2024-01-15 09:44:54 +0200

    ✅ Added tests for Result mothods: ok,err,map

--- libs/core/src/lib/Result.spec.ts
+++ libs/core/src/lib/Result.spec.ts
@@ -1,74 +1,97 @@
+/* tslint:disable:max-line-length */
+import { None, Some } from './Option';
 import { Err, Ok, ResultType } from './Result';
 
 describe('Result tests', () => {
   describe.each([
-    [new Ok(1), ResultType.Ok, true, false, 1, null, 'Ok(1)'],
+    [new Ok(1), ResultType.Ok, true, 1, null, 'Ok(1)'],
     [
       new Err('Expected'),
       ResultType.Err,
       false,
-      true,
       null,
       'Expected',
       'Err(Expected)',
     ],
     [
-      new Err(new Error('Expected')),
+      new Err(new Error('Expected nested')),
       ResultType.Err,
       false,
-      true,
       null,
-      new Error('Expected'),
-      'Err(Error: Expected)',
+      new Error('Expected nested'),
+      'Err(Error: Expected nested)',
     ],
-  ])(
-    'Result %s',
-    (result, type, isOk, isErr, unwrapped, error, stringified) => {
-      it(`[type] should be "${ResultType[type]}"`, () => {
-        expect(result.type).toBe(type);
-      });
+  ])('Result %s', (result, type, isOk, unwrapped, error, stringified) => {
+    it(`[type] should be "${ResultType[type]}"`, () => {
+      expect(result.type).toBe(type);
+    });
 
-      it(`[isOk] should be "${isOk}"`, () => {
-        expect(result.isOk()).toBe(isOk);
-      });
+    it(`[isOk] should be "${isOk}"`, () => {
+      expect(result.isOk()).toBe(isOk);
+    });
 
-      it(`[isErr] should be "${isErr}"`, () => {
-        expect(result.isErr()).toBe(isErr);
-      });
+    const positivePredicate = <T>(value: T) => isOk && value === unwrapped;
 
-      it(`[isOkAnd(positive predicate)] should be "${
-        isOk && unwrapped === 1
-      }"`, () => {
-        expect(result.isOkAnd((value) => value === 1)).toBe(
-          isOk && unwrapped === 1
-        );
-      });
+    it(`[isOkAnd(positive predicate)] should be "${positivePredicate(
+      unwrapped
+    )}"`, () => {
+      expect(result.isOkAnd(positivePredicate)).toBe(
+        positivePredicate(unwrapped)
+      );
+    });
 
-      const predicatedValue = unwrapped === 2;
+    const negativePredicate = <T>(value: T) => isOk && value !== unwrapped;
 
-      it(`[isOkAnd(negative predicate)] should be "${predicatedValue}"`, () => {
-        expect(result.isOkAnd((value) => value === 2)).toBe(predicatedValue);
-      });
+    it(`[isOkAnd(negative predicate)] should be "${negativePredicate(
+      unwrapped
+    )}"`, () => {
+      expect(result.isOkAnd(negativePredicate)).toBe(
+        negativePredicate(unwrapped)
+      );
+    });
 
-      //  TODO: dont like manual predicate, in case of adding new test cases we will need to manually add new predicate option
-      const predicatedError =
-        error === 'Expected' || error === new Error('Expected');
+    it(`[isErr] should be "${!isOk}"`, () => {
+      expect(result.isErr()).toBe(!isOk);
+    });
 
-      it(`[isErrAnd(positive predicate)] should be "${predicatedError}"`, () => {
-        expect(result.isErrAnd((err) => err === error)).toBe(predicatedError);
-      });
+    //  TODO: dont like manual predicate, in case of adding new test cases we will need to manually add new predicate option
+    const errorPredicate = <T>(err: T) =>
+      !isOk && (err === 'Expected' || err === new Error('Expected nested'));
 
-      it(`should unwrap to ${isOk ? 'the value' : 'throw'}`, () => {
-        if (isOk) {
-          expect(result.unwrap()).toBe(unwrapped);
-        } else {
-          expect(() => result.unwrap()).toThrow('Expected');
-        }
-      });
+    it(`[isErrAnd(positive predicate)] should be "${errorPredicate(
+      error
+    )}"`, () => {
+      expect(result.isErrAnd(errorPredicate)).toBe(errorPredicate(error));
+    });
 
-      it(`should stringify to ${stringified}`, () => {
-        expect(String(result)).toBe(String(stringified));
-      });
-    }
-  );
+    it(`[ok()] should be ${isOk ? `Some(${unwrapped})` : 'None'}`, () => {
+      expect(result.ok()).toStrictEqual(
+        isOk ? new Some(unwrapped) : new None()
+      );
+    });
+
+    it(`[err()] should be ${isOk ? 'None' : `Some(${error})`}`, () => {
+      expect(result.err()).toStrictEqual(isOk ? new None() : new Some(error));
+    });
+
+    it(`[unwrap()] should ${isOk ? `be ${result}` : 'throw'}`, () => {
+      if (isOk) {
+        expect(result.unwrap()).toBe(unwrapped);
+      } else {
+        expect(() => result.unwrap()).toThrow('Expected');
+      }
+    });
+
+    it(`[map(x => x + 1)] should be ${
+      isOk ? new Ok(unwrapped! + 1) : new Err(error)
+    }`, () => {
+      expect(result.map((value) => value + 1)).toStrictEqual(
+        isOk ? new Ok(unwrapped! + 1) : new Err(error)
+      );
+    });
+
+    it(`should stringify to ${stringified}`, () => {
+      expect(String(result)).toBe(String(stringified));
+    });
+  });
 });
